2cc88642f740ace2d7b498652c164204db28965d
Adam Schaible <44045097+Schibes@users.noreply.github.com>
2019-08-01 12:03:44 -0400

    added comments

--- python/cc-validate.py
+++ python/cc-validate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+#Imports the Python regular expression module
 import re
 
+#Function that checks if the first digit of the credit card number is good
 def is_good_start_digit(my_cc):
   startdigit_pattern = r'^[4-6]'
   if re.match(startdigit_pattern, my_cc):
@@ -9,6 +11,7 @@ def is_good_start_digit(my_cc):
   else:
     return False
 
+#Function that checks is the credit card number is the correct length, allowing for dashes
 def is_good_cc_length(my_cc):
   alldigits_pattern = r'\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d'
   dash_pattern = r'\d\d\d\d-\d\d\d\d-\d\d\d\d-\d\d\d\d'
@@ -20,6 +23,7 @@ def is_good_cc_length(my_cc):
     else:
       return False
 
+#Function that checks if the credit card number does not exceed the maximum of allowed repeating digits (3)
 def is_good_nonrepeating_digits(my_cc):
   repeat_pattern = r'(\d)\1{3}'
   my_cc_digits = re.sub('[^0-9]','', my_cc)
@@ -28,6 +32,7 @@ def is_good_nonrepeating_digits(my_cc):
   else:
     return True
 
+#Main script execution segment
 my_file = open('cc-list.txt', 'r')
 cc_num = 0
 cc_count = int(my_file.readline())
